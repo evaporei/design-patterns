00d2168ac50de54c9a691ee91b1710ee59f347ff
Hoang Phung <hoangpt@kaopiz.com>
2018-10-03 13:13:04 +0700

    implement singleton pattern in java

---
+++ creational/singleton/java/singleton.java
@@ -0,0 +1,14 @@
+package com.hoangpt.singleton;
+
+public class Singleton {
+
+    private Singleton() {}
+    
+    private static class SingletonHelper {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+    
+    public static Singleton getInstance() {
+        return SingletonHelper.INSTANCE;
+    }
+}
